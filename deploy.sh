8e1b9c3a5bb598802086d3703776b395693c8d63
Guy Dunton <guy.dunton@gmail.com>
2018-01-07 11:17:24 +0000

    edit the travis deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
+#!/bin/sh
 cp index.html build/index.html
 mkdir build/styles
 cp styles/style.css build/styles/style.css
-sed -i 's/<!-- base placeholder -->/<base href=\"www.guydunton.com\/gb-sprite-editor\"\/>/g' build/index.html
+cd build
+sed -i 's/<!-- base placeholder -->/<base href=\"www.guydunton.com\/gb-sprite-editor\"\/>/g' index.html
+cd ..
