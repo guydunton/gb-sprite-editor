3d5710c8d2787a6e7954489b4af691c8efa8c13d
Guy Dunton <guy.dunton@gmail.com>
2018-01-08 09:07:08 +0000

    remove the relative paths for deployment

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cp index.html build/index.html
-mkdir build/styles
-cp styles/style.css build/styles/style.css
-cd build
-sed -i 's/<!-- base placeholder -->/<base href=\"https://www.guydunton.com\/gb-sprite-editor\/\"\/>/g' index.html
-cd ..
